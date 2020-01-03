159cc569873fbf5a0def0ad6cb3523f63f5746b7
tanfly <mcewen.stella@gmail.com>
2020-01-03 12:00:42 -0600

    abbreviated sort photo code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,71 +20,12 @@ class ApplicationController < ActionController::Base
   end
 
   def high_to_low(photos)
-    ids = photos.collect do |photo|
-      photo.id
-    end
-
-    p = Pat.group(:photo_id).count
-
-    valid_ids = p.reject!{|id| !ids.include?(id)}
-
-    if valid_ids
-    sorted_photo_pats = valid_ids.sort_by{|k, v| v}.reverse
-    else
-      sorted_photo_pats = p.sort_by{|k, v| v}.reverse
-    end
-
-    photo_ids_pats = sorted_photo_pats.collect do |photo_id, pat_count|
-      photo_id
-    end
-
-    photos_w_zero = photos.reject{|photo| photo_ids_pats.include?(photo.id)}
-    
-    photo_ids_zero = photos_w_zero.collect do |photo|
-      photo.id 
-    end
-
-    all_photo_ids = photo_ids_pats + photo_ids_zero
-
-    @photos = all_photo_ids.collect do |id|
-      Photo.find(id)
-    end
-
+   @photos = photos.sort_by{|photo| photo.pats.count}.reverse
   end
 
 
   def low_to_high(photos)
-
-    ids = photos.collect do |photo|
-      photo.id
-    end
-
-    p = Pat.group(:photo_id).count
-
-    valid_ids = p.reject!{|id| !ids.include?(id)}
-
-    if valid_ids
-      sorted_photo_pats = valid_ids.sort_by{|k, v| v}
-    else
-        sorted_photo_pats = p.sort_by{|k, v| v}
-    end
-
-    photo_ids_pats = sorted_photo_pats.collect do |photo_id, pat_count|
-      photo_id
-    end
-
-    photos_w_zero = photos.reject{|photo| photo_ids_pats.include?(photo.id)}
-                
-    photo_ids_zero = photos_w_zero.collect do |photo|
-      photo.id 
-    end
-    
-    all_photo_ids = photo_ids_zero + photo_ids_pats
-
-    @photos = all_photo_ids.collect do |id|
-      Photo.find(id)
-    end
-
+    @photos = photos.sort_by{|photo| photo.pats.count}
   end
 
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -9,3 +9,4 @@ class Photo < ApplicationRecord
     validates_presence_of :image
     mount_uploader :image, PhotoUploader
 end
+ 
